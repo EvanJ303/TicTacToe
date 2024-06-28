eae710565fd45a5e1fd0e1cd1be26d5ebb71892c
EvanJ303 <105998830+EvanJ303@users.noreply.github.com>
2024-06-28 09:52:10 -0400

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,27 +1,18 @@
-rowone=[0,0,0]
-rowtwo=[0,0,0]
-rowthree=[0,0,0]
-colone=[rowone[0],rowtwo[0],rowthree[0]]
-coltwo=[rowone[1],rowtwo[1],rowthree[1]]
-colthree=[rowone[2],rowtwo[2],rowthree[2]]
-diagone=[rowone[0],rowtwo[1],rowthree[2]]
-diagtwo=[rowone[2],rowtwo[1],rowthree[0]]
-
 player=1
 playab=1
 
-gameboard=[rowone[0],rowone[1],rowone[2],rowtwo[0],rowtwo[1],rowtwo[2],rowthree[0],rowthree[1],rowthree[2]]
+gameboard=[0,0,0,0,0,0,0,0,0]
 
-print(rowone)
-print(rowtwo)
-print(rowthree)
+print("[0,0,0]")
+print("[0,0,0]")
+print("[0,0,0]")
 
 while playab==1:
     if player==1:
         print("P1 TURN")
     else:
         print("P2 TURN")
-    punchcount=int(input("SELECT A NUMBER 0-9"))
+    punchcount=int(input("SELECT A NUMBER 0-8"))
     if player==1:
         gameboard[punchcount]=1
     else:
@@ -29,17 +20,48 @@ while playab==1:
     rowone=[gameboard[0],gameboard[1],gameboard[2]]
     rowtwo=[gameboard[3],gameboard[4],gameboard[5]]
     rowthree=[gameboard[6],gameboard[7],gameboard[8]]
-    colone=[rowone[0],rowtwo[0],rowthree[0]]
-    coltwo=[rowone[1],rowtwo[1],rowthree[1]]
-    colthree=[rowone[2],rowtwo[2],rowthree[2]]
-    diagone=[rowone[0],rowtwo[1],rowthree[2]]
-    diagtwo=[rowone[2],rowtwo[1],rowthree[0]]
+    colone=[gameboard[0],gameboard[3],gameboard[6]]
+    coltwo=[gameboard[1],gameboard[4],gameboard[7]]
+    colthree=[gameboard[2],gameboard[5],gameboard[8]]
+    diagone=[gameboard[0],gameboard[4],gameboard[8]]
+    diagtwo=[gameboard[2],gameboard[4],gameboard[6]]
     print(rowone)
     print(rowtwo)
     print(rowthree)
-    if rowone or rowtwo or rowthree or colone or coltwo or colthree or diagone or diagtwo==[1,1,1]or[2,2,2]:
+    if rowone==[1,1,1]:
         break
-    
+    if rowtwo==[1,1,1]:
+        break
+    if rowthree==[1,1,1]:
+        break
+    if colone==[1,1,1]:
+        break
+    if coltwo==[1,1,1]:
+        break
+    if colthree==[1,1,1]:
+        break
+    if diagone==[1,1,1]:
+        break
+    if diagtwo==[1,1,1]:
+        break
+    if rowone==[2,2,2]:
+        break
+    if rowtwo==[2,2,2]:
+        break
+    if rowthree==[2,2,2]:
+        break
+    if colone==[2,2,2]:
+        break
+    if coltwo==[2,2,2]:
+        break
+    if colthree==[2,2,2]:
+        break
+    if diagone==[2,2,2]:
+        break
+    if diagtwo==[2,2,2]:
+        break
+    player=(player%2)+1
+   
 playab=2
 if player==1:
     print("P1 WINS")
