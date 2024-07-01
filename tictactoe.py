d38696eaca81ab90abb8d5842e9c6a07f68dbec0
EvanJ303 <105998830+EvanJ303@users.noreply.github.com>
2024-07-01 10:58:15 -0400

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,68 +1,42 @@
-player=1
-playab=1
+board=[[0,0,0],[0,0,0],[0,0,0]]
 
-gameboard=[0,0,0,0,0,0,0,0,0]
+C1=[board[0][0],board[1][0],board[2][0]]
+C2=[board[0][1],board[1][1],board[2][1]]
+C3=[board[0][2],board[1][2],board[2][2]]
+D1=[board[0][0],board[1][1],board[2][2]]
+D2=[board[0][2],board[1][1],board[2][0]]
 
-print("[0,0,0]")
-print("[0,0,0]")
-print("[0,0,0]")
+inviboard=[board[0],board[1],board[2],C1,C2,C3,D1,D2]
+player=2
+playstop=0
 
-while playab==1:
+# Print Board
+for i in board:
+    print(i)
+
+while playstop==0:
+    player=(player%2)+1
     if player==1:
         print("P1 TURN")
     else:
         print("P2 TURN")
-    punchcount=int(input("SELECT A NUMBER 0-8"))
+    selrow=int(input("SELECT ROW"))
+    selcol=int(input("SELECT COLUMN"))
     if player==1:
-        gameboard[punchcount]=1
+        board[selrow][selcol]=1
     else:
-        gameboard[punchcount]=2
-    rowone=[gameboard[0],gameboard[1],gameboard[2]]
-    rowtwo=[gameboard[3],gameboard[4],gameboard[5]]
-    rowthree=[gameboard[6],gameboard[7],gameboard[8]]
-    colone=[gameboard[0],gameboard[3],gameboard[6]]
-    coltwo=[gameboard[1],gameboard[4],gameboard[7]]
-    colthree=[gameboard[2],gameboard[5],gameboard[8]]
-    diagone=[gameboard[0],gameboard[4],gameboard[8]]
-    diagtwo=[gameboard[2],gameboard[4],gameboard[6]]
-    print(rowone)
-    print(rowtwo)
-    print(rowthree)
-    if rowone==[1,1,1]:
-        break
-    if rowtwo==[1,1,1]:
-        break
-    if rowthree==[1,1,1]:
-        break
-    if colone==[1,1,1]:
-        break
-    if coltwo==[1,1,1]:
-        break
-    if colthree==[1,1,1]:
-        break
-    if diagone==[1,1,1]:
-        break
-    if diagtwo==[1,1,1]:
-        break
-    if rowone==[2,2,2]:
-        break
-    if rowtwo==[2,2,2]:
-        break
-    if rowthree==[2,2,2]:
-        break
-    if colone==[2,2,2]:
-        break
-    if coltwo==[2,2,2]:
-        break
-    if colthree==[2,2,2]:
-        break
-    if diagone==[2,2,2]:
-        break
-    if diagtwo==[2,2,2]:
-        break
-    player=(player%2)+1
-   
-playab=2
+        board[selrow][selcol]=2
+    C1=[board[0][0],board[1][0],board[2][0]]
+    C2=[board[0][1],board[1][1],board[2][1]]
+    C3=[board[0][2],board[1][2],board[2][2]]
+    D1=[board[0][0],board[1][1],board[2][2]]
+    D2=[board[0][2],board[1][1],board[2][0]]
+    for n in board:
+        print(n)
+    for k in range(0,8):
+        if inviboard[k]==[1,1,1] or inviboard[k]==[2,2,2]:
+            playstop=1
+
 if player==1:
     print("P1 WINS")
 else:
